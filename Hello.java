6e2537970ef99f24af1a163333d1ce5b0adb9ac9
stefanoromei <73590467+stefanoromei@users.noreply.github.com>
2021-10-20 12:48:34 +0200

    commit Hello.java #3

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello {
 	int i;
 	public static void main(String[] args) {
 		// Prints "Hello, world" to the terminal window.
-		System.out.println("Hello, World");
+		logger.log("Hello, World");
 	}
 } 
